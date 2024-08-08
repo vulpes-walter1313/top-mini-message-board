f7994bac8d5c7341e6849af1d9a099c2c837e1d7
Walter Fritsche <wfritsche95@protonmail.com>
2024-08-08 09:59:32 -0500

    added form routes, link, and styles

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,23 @@ const messages = [
 ];
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Message Board", messages });
 });
+
+// GET new message form
+router
+  .route("/new")
+  .get(function (req, res, next) {
+    res.render("form", { title: "New Message", messages });
+  })
+  .post(function (req, res) {
+    console.log(`Name: ${req.body.name} message: ${req.body.message}`)
+    messages.push({
+      text: req.body.message,
+      user: req.body.name,
+      added: new Date(),
+    });
+    res.redirect("/");
+  });
 
 module.exports = router;
